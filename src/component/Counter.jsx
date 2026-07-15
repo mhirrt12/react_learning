1c7d2d03122a8c42817b089702c97f8b90d7e4b3
mhirrt12 <kaletamene90@gmail.com>
2026-07-15 00:20:44 -0700

    Add Reset button to Counter component

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -6,6 +6,7 @@ const Counter = () => {
     <div>
       <h1>Counter: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(0)}>Reset</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   )
