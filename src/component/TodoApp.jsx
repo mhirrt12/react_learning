d344d28fdf71eb8e61fc051e0f69f5ead3e47971
mhirrt12 <kaletamene90@gmail.com>
2026-07-15 04:12:38 -0700

    Refactor TodoApp component to initialize todos state and implement addTodo functionality

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 const TodoApp = () => {
-    const [todos, setTodos] = useState([
-       
-    ]);
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState("");
+    const addTodo = () => {
+        if (input.trim() === "") return;
+
+  setTodos([
+    ...todos,
+    input
+  ]);
+
+  setInput("");
+
+};
 
     return (
         <>
+            <input
+  type="text"
+  value={input}
+  onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={addTodo}>
+  Add
+</button>
            {todos.length===0?<p> not todo list found</p>:<ul>
                 {todos.map((todo, index) => {
                     return (
