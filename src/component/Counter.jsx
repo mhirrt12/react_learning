ce821b442c33b77d8dbaa06219f70df272a5f0a0
mhirrt12 <kaletamene90@gmail.com>
2026-07-17 06:31:43 -0700

    Refactor Counter component to implement useEffect for rendering log and simplify structure

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,15 +1,41 @@
-import {useState} from 'react'
+// import {useState} from 'react'
 
-const Counter = () => {
-  const [count, setCount] = useState(0)
-  return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-    </div>
-  )
+// const Counter = () => {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div>
+//       <h1>Counter: {count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//       <button onClick={() => setCount(0)}>Reset</button>
+//       <button onClick={() => setCount(count - 1)}>Decrement</button>
+//     </div>
+//   )
+// }
+
+// export default Counterimport { useState, useEffect } from "react";
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+
+        console.log("Rendered!");
+
+    });
+
+    return (
+
+        <>
+            <h1>{count}</h1>
+
+            <button
+                onClick={() => setCount(count + 1)}
+            >
+                +
+            </button>
+        </>
+
+    );
+
 }
-
-export default Counter
