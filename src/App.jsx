b36224a2b8cdeede76ab706cbaeb35caf0e0057c
mhirrt12 <kaletamene90@gmail.com>
2026-07-18 08:16:50 -0700

    Add LoginSuccess component to App for displaying login success state

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,16 @@ import About from './component/pages/About'
 import Contact from './component/pages/Contact'
 import { Routes, Route } from 'react-router-dom'
 import StudentDetails from './component/StudentDetails'
+import LoginSuccess from './component/LoginSuccess'
 function App() {
  
 
   return (
     <> 
       <Navbar />
+        <LoginSuccess />
       <Routes >
+      
         <Route path="/student/:id" element={<StudentDetails />} />
          <Route
         path="/"
