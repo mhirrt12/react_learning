3ff2c5c13995109c937a55b1a5bf014936982c3c
mhirrt12 <kaletamene90@gmail.com>
2026-07-18 07:35:20 -0700

    Add student links to Navbar for quick access to student profiles

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,6 +5,11 @@ const Navbar = () => {
     <Link to="/">Home</Link>
     <Link to="/about">About</Link>
     <Link to="/contact">Contact</Link>
+    <Link to="/student/1">Student 1</Link>
+
+    <Link to="/student/2">Student 2</Link>
+
+       <Link to="/student/3">Student 3</Link>
 </nav>
 
     )
