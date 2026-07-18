fa1aea83db18a702e2c41cb6dfef2ca399b716ff
mhirrt12 <kaletamene90@gmail.com>
2026-07-18 04:48:12 -0700

    Refactor Navbar component to use React Router Links for navigation

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,11 @@
-const Navbar=()=>{
-    return (<h1>
-        Navbar
-    </h1>
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    return (
+        <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+</nav>
 
     )
 };
