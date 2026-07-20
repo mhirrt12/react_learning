3da19d4a4701bc6dbb866ffb8b745e50d5ec9b1f
mhirrt12 <kaletamene90@gmail.com>
2026-07-20 00:31:56 -0700

    Refactor App component to remove unused imports and simplify structure for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,23 @@
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
 
-import './App.css'
-import Footer from './component/Footer'
-import StudentCard from './component/StudentCard'
-import Hero from './component/Hero'
-import Navbar from './component/Navbar'
-import CourseCard from './component/CourseCard'
-import Counter from './component/Counter'
-import LikeButton from './component/LikeButton'
-import Login from './component/Login'
-import ShowHide from './component/ShowHide'
-import ThemeToggle from './component/ThemeToggle'
-import TodoApp from './component/TodoApp'
-import App2 from './component/App2'
-import PageTitle from './component/PageTitle'
-import Users from './component/Users'
-import Posts from './component/Posts'
-import CreatePost from './component/CreatePost'
-import Home from './component/pages/Home'
-import About from './component/pages/About'
-import Contact from './component/pages/Contact'
-import { Routes, Route } from 'react-router-dom'
-import StudentDetails from './component/StudentDetails'
-import LoginSuccess from './component/LoginSuccess'
 function App() {
- 
+  const { isDark } = useContext(ThemeContext);
 
   return (
-    <> 
-      <Navbar />
-        <LoginSuccess />
-      <Routes >
-      
-        <Route path="/student/:id" element={<StudentDetails />} />
-         <Route
-        path="/"
-        element={<Home />}
-        />
-          <Route
-        path="/about"
-        element={<About />}
-        />
-          <Route
-        path="/contact"
-        element={<Contact />}
-    />
-      </Routes>
-      <CreatePost />
-      <Posts />
-       {/* <Users /> */}
-      <PageTitle />
-     
-      
-<App2 />
-      <TodoApp />
-      <Login />
+    <div
+      style={{
+        backgroundColor: isDark ? "#222" : "#fff",
+        color: isDark ? "white" : "black",
+        minHeight: "100vh",
+        padding: "20px",
+      }}
+    >
       <ThemeToggle />
-      <ShowHide />
-      <LikeButton />
-      <h1>Hello React!</h1>
-      <Counter />
-        <StudentCard />
-         {/* <StudentCard name="sara"
-             department="Software Engineering" />
-          <StudentCard name="beti"
-             department="Software Engineering"/> */}
-      <Hero />
-      <CourseCard />
-
-{/* <CourseCard
-  courseName="JavaScript"
-  instructor="Sara"
-  duration="6 Weeks"
-/>
-
-<CourseCard
-  courseName="Node.js"
-  instructor="Mike"
-  duration="10 Weeks"
-/> */}
-     <Footer />
-    </>
-  )
+      <h1>Context API Theme App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
