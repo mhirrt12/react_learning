3644bda72c093a41752639dfdb6feaa5de2ae745
mhirrt12 <kaletamene90@gmail.com>
2026-07-22 03:07:14 -0700

    Refactor main.jsx to integrate Redux Provider and remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// import { ThemeProvider } from "./context/ThemeContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <ThemeProvider>
+//         <App />
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+
+<Provider store={store}>
+    <App />
+</Provider>
